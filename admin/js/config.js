f6bf1f086b22695ce28abe2ae2c6c96f5834050d
Mhmd Sibhathulla <sibhathullamkd@gmail.com>
2026-05-22 17:46:53 +0530

    Update config.js

--- admin/js/config.js
+++ admin/js/config.js
@@ -1,8 +1,8 @@
-var SUPABASE_URL = 'https://zrdnishiposoqeibpkjo.supabase.co';
-var SUPABASE_ANON_KEY = 'sb_publishable_0yeM3W6jMofbPoHvgxchoA_0dU-iUo8';
+var SUPABASE_URL = 'https://ftnykcpmtwusryrivvwe.supabase.co';
+var SUPABASE_ANON_KEY = 'sb_publishable_Z3RDyV6ZIwZ09EhfHMjZrA_WfXOI1KF';
 var ADMIN_USERNAME = 'admin';
 var ADMIN_PASSWORD = 'fhzadmin2026';
-var SITE_URL = 'https://sibhathullamkd.github.io/Flarrd';
+var SITE_URL = 'https://flarrd.pages.dev/';
 
 if (!window._supabaseClient) {
   window._supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
